93863b92a1dc40348b783dd317aa97b5c073fc92
Alexandre COURBOIS <courbois.alexandre76440@gmail.com>
2019-12-10 12:48:52 +0100

    ajout function pour dire si l'année est bisextile ou pas

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,6 +28,7 @@ document.write('Je fais du JS');
 
 /*Ici on affiches tout les multiples de 9 de -1722 à 2756*/
 
+/*
 let message = '<ul>';
 
 for (let i = -1722 ; i <= 2756 ; i++) {
@@ -41,11 +42,44 @@ for (let i = -1722 ; i <= 2756 ; i++) {
 
 message += '</ul>';
 
-/*Alert à la même utilité que le Die*/
+/!*Alert à la même utilité que le Die*!/
 alert('Aye !')
 
 document.write(message);
+*/
 
+/* On initialise les variables avec prompt qui fait pop une fenetre pour rentrer un truck :
+let valeur =  prompt('Veuillez saisir un nom')
+let msg = '';
 
+console.log(valeur);
 
+On vérifie que isNaN (C'est à dire on vérifie que c'est un nombre ou pas) :
+
+if (!isNaN(valeur)) {
+
+    if(valeur > 0) { msg = 'Positif'; }
+    else if(valeur <0) { msg ='Nefatif'; }
+    else { msg ='Nul !'}
+
+}
+
+else msg = 'Un nombre j\'ai dit';
+
+On ressort la valeur ci dessous :
+
+document.write(msg);
+*/
+
+/*.On recupères l'id et on lui définit un type text*/
+
+document.getElementById('truc').innerText = 'Coucou';
+
+/*On récupères l'id et on lui définit une couleur via l'attribut style qui dit qu'on fait du css */
+
+document.getElementById('truc').style.color = '#FF0000';
+
+/*On récupères l'id et on lui définit un background color via l'attribut style qui dit qu'on fait du css */
+
+document.getElementById('truc').style.backgroundColor = '#000';
 
